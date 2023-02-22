db670cc7065b351f7723033531b61ad2b5f776a6
CJskii <czareczarek3@gmail.com>
2023-02-22 00:59:59 +0000

    Navbar component

---
+++ speedrun-ethereum/src/Components/CustomButton.tsx
@@ -0,0 +1,9 @@
+const CustomButton = () => {
+  return (
+    <button className="btn bg-gradient-to-r from-primary to-secondary">
+      Connect Wallet
+    </button>
+  );
+};
+
+export default CustomButton;
--- speedrun-ethereum/src/pages/index.tsx
+++ speedrun-ethereum/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import Navbar from "@/Components/Navbar";
+import Hero from "@/Components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-black min-h-screen">
-        <h1 className="text-4xl text-center text-white p-8">Hello World!</h1>
-      </main>
+      <Navbar />
     </>
   );
 }
