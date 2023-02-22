cdfbb4942bd36ab82d213d8e92d72b95db0a93db
CJskii <czareczarek3@gmail.com>
2023-02-22 20:42:14 +0000

    Fixed Font

--- speedrun-ethereum/src/Components/Hero.tsx
+++ speedrun-ethereum/src/Components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div className="flex relative flex-col justify-center items-center min-h-full lg:w-1/2">
-      <h1 className="text-4xl text-center text-base-content px-4 font-bold text-5xl sm:text-7xl">
+      <h1 className="text-center text-base-content px-4 font-bold text-5xl sm:text-7xl">
         Let&apos;s Build dapps On Ethereum
       </h1>
       <div className="w-full flex justify-start items-center pb-4 p-2">
@@ -17,7 +17,8 @@ const Hero = () => {
         />
       </div>
       <p className="text-md text-center px-4 text-lg sm:text-xl ">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis fugit expedita doloremque nulla enim iure!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
+        fugit expedita doloremque nulla enim iure!
       </p>
       <div className="w-full flex justify-end">
         <Image className="" src={heroImg} alt="hero" width={200} height={200} />
--- speedrun-ethereum/src/Components/Team.tsx
+++ speedrun-ethereum/src/Components/Team.tsx
@@ -3,7 +3,7 @@ import TeamCard from "./TeamCard";
 const Team = () => {
   return (
     <div className="flex flex-col justify-center items-center lg:w-4/6 gap-16 my-16">
-      <h1 className="text-center text-4xl">Our Team</h1>
+      <h1 className="text-center font-bold text-5xl sm:text-7xl">Our Team</h1>
       <div className="flex justify-center items-center gap-16 max-md:flex-col">
         {/* ----- Card 1 ----- */}
         <TeamCard />
