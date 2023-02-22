eae1785d0348d5fe5db2e2db710b6011f3d45eaf
CJskii <czareczarek3@gmail.com>
2023-02-22 00:00:38 +0000

    theme + packages

--- speedrun-ethereum/tailwind.config.js
+++ speedrun-ethereum/tailwind.config.js
@@ -5,4 +5,18 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#8C02F9C2",
+          secondary: "#1C69FFC2",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#030709",
+          "base-content": "#ffffff",
+        },
+      },
+    ],
+  },
 };
