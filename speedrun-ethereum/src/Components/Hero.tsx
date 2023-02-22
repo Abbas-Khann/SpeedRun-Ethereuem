effdf7d394d88fb5d7bcdaf13df8e8acba43c2cd
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-02-22 13:17:52 +0800

    Fixed hero section

--- speedrun-ethereum/src/Components/Hero.tsx
+++ speedrun-ethereum/src/Components/Hero.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable react/no-unescaped-entities */
 import heroImg from "../../public/img/Hero/variant1.svg";
 import heroImg2 from "../../public/img/Hero/variant2.svg";
 import Image from "next/image";
 const Hero = () => {
   return (
     <div className="flex relative flex-col justify-center items-center min-h-full lg:w-1/2">
-      <h1 className="text-4xl text-center text-base-content px-4">
-        Let's Learn How To Build On Ethereum
+      <h1 className="text-4xl text-center text-base-content px-4 font-bold text-5xl sm:text-7xl">
+        Let&apos;s Build dapps On Ethereum
       </h1>
       <div className="w-full flex justify-start items-center pb-4 p-2">
         <Image
@@ -17,16 +16,14 @@ const Hero = () => {
           height={200}
         />
       </div>
-      <p className="text-md text-center px-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-        praesentium quo eum ducimus a inventore odio, expedita ratione
-        doloremque!
+      <p className="text-md text-center px-4 text-lg sm:text-xl ">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis fugit expedita doloremque nulla enim iure!
       </p>
       <div className="w-full flex justify-end">
         <Image className="" src={heroImg} alt="hero" width={200} height={200} />
       </div>
       {/* Add scroll function */}
-      <button className="btn mt-16 bg-transparent">
+      <button className="mt-10 bg-transparent border-2 px-6 py-2 rounded-sm hover:bg-gradient-to-r from-primary to-secondary hover:animate-bounce">
         <a href="#solutions">Get Started</a>
       </button>
     </div>
--- speedrun-ethereum/src/pages/_app.tsx
+++ speedrun-ethereum/src/pages/_app.tsx
@@ -4,7 +4,7 @@ import { Lato } from '@next/font/google'
 
 const lato = Lato({
   subsets: ['latin'],
-  weight : ["300", "400", "700"]
+  weight : ["300", "400", "700", "900"]
 })
 
 export default function App({ Component, pageProps }: AppProps) {
