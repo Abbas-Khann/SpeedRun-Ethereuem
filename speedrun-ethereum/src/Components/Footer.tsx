5f37eb16244e49301ba2815a78f3727ab935cfc6
CJskii <czareczarek3@gmail.com>
2023-02-23 18:06:14 +0000

    Footer noreferrer

--- speedrun-ethereum/src/Components/Footer.tsx
+++ speedrun-ethereum/src/Components/Footer.tsx
@@ -4,14 +4,18 @@ const Footer = () => {
     <footer className="footer footer-center p-4 bg-gradient-to-r from-primary to-secondary text-base-content h-[88px]">
       <div>
         <p className={`text-xl ${styles.rainbow}`}>
-          Built with 🤍 by <a
-          href="https://github.com/Abbas-Khann" 
-          target="_blank">
-            Abbas Khan </a>
-             & <a
-          href="https://github.com/CJskii" 
-          target="_blank">
-            CJski</a>
+          Built with 🤍 by{" "}
+          <a
+            href="https://github.com/Abbas-Khann"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Abbas Khan{" "}
+          </a>
+          &{" "}
+          <a href="https://github.com/CJskii" target="_blank" rel="noreferrer">
+            CJski
+          </a>
         </p>
       </div>
     </footer>
