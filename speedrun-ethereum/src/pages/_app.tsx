76690938bad9286514cda0ccc7d2dd8b270a530c
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2023-02-23 18:19:00 +0800

    Added wallet connection and custom button

--- speedrun-ethereum/src/pages/_app.tsx
+++ speedrun-ethereum/src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Lato } from '@next/font/google'
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  midnightTheme
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, goerli, WagmiConfig } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, provider } = configureChains(
+  [goerli],
+  [
+    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'SpeedRun Ethereum',
+  chains
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 const lato = Lato({
   subsets: ['latin'],
@@ -10,7 +35,11 @@ const lato = Lato({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={lato.className}>
+      <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={midnightTheme()} modalSize="compact" >
       <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
     </main>
   )
   
