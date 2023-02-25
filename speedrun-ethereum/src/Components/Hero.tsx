264b7d52fb10ec01135a5bf9295ee4c8bd4beb79
CJskii <czareczarek3@gmail.com>
2023-02-25 01:46:33 +0000

    Added LG breakpoints

--- speedrun-ethereum/src/Components/Hero.tsx
+++ speedrun-ethereum/src/Components/Hero.tsx
@@ -5,12 +5,12 @@ import styles from "../styles/Grid.module.css";
 const Hero = () => {
   return (
     <div
-      className={`flex relative flex-col justify-center items-center min-h-full lg:grid lg:w-1/2 ${styles.grid}`}
+      className={`flex relative flex-col justify-center items-center min-h-full lg:grid lg:w-1/2 md:pb-8 ${styles.grid}`}
     >
-      <h1 className="text-center text-base-content px-4 font-bold text-5xl sm:text-7xl lg:col-start-4 lg:col-span-6 lg:row-start-2 lg:row-span-2">
+      <h1 className="text-center text-base-content md:py-4 px-4 font-bold text-5xl sm:text-7xl col-start-4 col-span-6 row-start-3 row-span-2 lg:col-start-3 lg:col-span-8 lg:row-start-3 lg:row-span-3 place-self-center">
         Let&apos;s Build dApps On Ethereum
       </h1>
-      <div className="w-full flex justify-start items-center pb-4 p-2 lg:w-[33vw] lg:col-start-1 lg:col-span-6 lg:row-start-2 lg:row-span-2 lg:-ml-16 lg:pt-8 ">
+      <div className="max-md:w-full md:w-4/6 md:self-start flex justify-start items-center pb-4 p-2 w-[30vw] col-start-1 col-span-3 row-start-2 row-span-3 min-xl:-ml-28 lg:row-start-4 lg:row-span-4 lg:col-start-1 lg:col-span-6 lg:w-[33vw] lg:-ml-16 ">
         <Image
           className="w-full"
           src={heroImg2}
@@ -19,11 +19,11 @@ const Hero = () => {
           height={200}
         />
       </div>
-      <p className="text-md text-center px-4 text-lg sm:text-xl lg:col-start-5 lg:col-span-4 lg:row-start-4 lg:row-span-2 min-[1024px]:row-start-5 lg:self-center">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
-        fugit expedita doloremque nulla enim iure!
+      <p className="text-md text-center max-lg:py-8 md:w-5/6 px-4 text-lg sm:text-xl col-start-5 col-span-4 row-start-5 row-span-3 lg:row-start-6 lg:col-start-5 lg:px-0 lg:col-span-5 lg:row-span-2 place-self-center lg:place-self-start">
+        Lorem ipsum dolor sit amet consectetur. Sed leo venenatis pellentesque
+        neque.
       </p>
-      <div className="w-full flex justify-end min-md:w-[33vw] lg:col-span-6 lg:col-start-9 lg:row-start-4 lg:row-span-3 lg:self-center">
+      <div className="max-md:w-full md:w-4/6 md:pb-8 flex justify-end w-[33vw] col-span-6 col-start-9 row-start-5 row-span-3 lg:self-center lg:row-start-6 lg:row-span-3 lg:col-start-8 lg:col-span-5 lg:w-[33vw] place-self-end lg:-mr-12">
         <Image
           className="w-full"
           src={heroImg}
@@ -33,7 +33,7 @@ const Hero = () => {
         />
       </div>
       {/* Add scroll function */}
-      <button className="mt-10 bg-transparent border-2 py-2 w-[180px] rounded-sm hover:bg-gradient-to-r from-primary to-secondary hover:animate-bounce row-start-6 col-start-6 col-span-2 self-center min-[1024px]:row-start-7">
+      <button className="mt-10 bg-transparent border-2 py-2 w-[180px] rounded-sm hover:bg-gradient-to-r from-primary to-secondary hover:animate-bounce row-start-5 col-start-6 col-span-2 place-self-center lg:row-start-7 lg:row-span-3 lg:col-start-6 lg:col-span-2 lg:self-center">
         <a href="#solutions">Get Started</a>
       </button>
     </div>
