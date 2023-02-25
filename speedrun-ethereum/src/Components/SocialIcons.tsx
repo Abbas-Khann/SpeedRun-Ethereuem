4eddb134fe082332b577a88c33630d06c8d0e9e8
CJskii <czareczarek3@gmail.com>
2023-02-25 21:32:06 +0000

    Team links + pictures

--- speedrun-ethereum/src/Components/SocialIcons.tsx
+++ speedrun-ethereum/src/Components/SocialIcons.tsx
@@ -2,41 +2,50 @@ import React from "react";
 import Image from "next/image";
 import TeamCard from "./TeamCard";
 const SocialIcons = ({
-  discord,
-  twitter,
-  github,
+  discordImg,
+  twitterImg,
+  githubImg,
+  discordLink,
+  twitterLink,
+  githubLink,
 }: {
-  discord: string;
-  twitter: string;
-  github: string;
+  discordImg: string;
+  twitterImg: string;
+  githubImg: string;
+  discordLink: string;
+  twitterLink: string;
+  githubLink: string;
 }) => {
   return (
     <div className="flex gap-4 p-4">
       <div className="bg-gradient-to-r from-primary to-secondary p-0.5 rounded-[6px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-150 cursor-pointer">
         <a
-          href="#"
+          href={discordLink}
           target="_blank"
           className="rounded-[6px] flex justify-center items-center h-[43px] w-[43px] bg-base-100"
+          rel="noreferrer"
         >
-          <Image src={discord} alt="discord" />
+          <Image src={discordImg} alt="discord" />
         </a>
       </div>
       <div className="bg-gradient-to-r from-primary to-secondary p-0.5 rounded-[6px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-150 cursor-pointer">
         <a
-          href="#"
+          href={twitterLink}
           target="_blank"
           className="rounded-[6px] flex justify-center items-center h-[43px] w-[43px] bg-base-100"
+          rel="noreferrer"
         >
-          <Image src={twitter} alt="discord" />
+          <Image src={twitterImg} alt="discord" />
         </a>
       </div>
       <div className="bg-gradient-to-r from-primary to-secondary p-0.5 rounded-[6px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-150 cursor-pointer">
         <a
-          href="#"
+          href={githubLink}
           target="_blank"
           className="rounded-[6px] flex justify-center items-center h-[43px] w-[43px] bg-base-100"
+          rel="noreferrer"
         >
-          <Image src={github} alt="discord" />
+          <Image src={githubImg} alt="discord" />
         </a>
       </div>
     </div>
--- speedrun-ethereum/src/Components/TeamCard.tsx
+++ speedrun-ethereum/src/Components/TeamCard.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import pfp from "../../public/img/cj-pfp.png";
-import discord from "../../public/img/Icons/discord.svg";
-import twitter from "../../public/img/Icons/twitter.svg";
-import github from "../../public/img/Icons/github.svg";
+import discordImg from "../../public/img/Icons/discord.svg";
+import twitterImg from "../../public/img/Icons/twitter.svg";
+import githubImg from "../../public/img/Icons/github.svg";
 import SocialIcons from "./SocialIcons";
 
-const TeamCard = () => {
+const TeamCard = ({ member }: { member: any }) => {
+  const { name, title, discord, twitter, github, pfp } = member;
   return (
     <div className="bg-gradient-to-r from-primary to-secondary p-0.5 rounded-[12px] relative">
       <div className="flex flex-col w-[350px] h-[325px] justify-evenly items-center rounded-[12px] bg-base-100 max-sm:w-[90vw]">
@@ -17,18 +17,25 @@ const TeamCard = () => {
               src={pfp}
               alt="pfp"
               className="h-[128px] w-[97px] rounded-[12px] pt-[2px] pl-[2px] pb-[6px] pr-[6px]"
+              width={97}
+              height={128}
             />
           </div>
 
           <div className="w-2/5">
-            <h1 className="text-white text-xl">Abbas Khan</h1>
-            <p className="text-white opacity-60 pt-1 text-sm">
-              Full Stack Developer
-            </p>
+            <h1 className="text-white text-xl">{name}</h1>
+            <p className="text-white opacity-60 pt-1 text-sm">{title}</p>
           </div>
         </div>
         {/* ------ Icons ----- */}
-        <SocialIcons discord={discord} twitter={twitter} github={github} />
+        <SocialIcons
+          discordImg={discordImg}
+          twitterImg={twitterImg}
+          githubImg={githubImg}
+          discordLink={discord}
+          twitterLink={twitter}
+          githubLink={github}
+        />
       </div>
     </div>
   );
